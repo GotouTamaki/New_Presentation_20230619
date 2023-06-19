669124c243520178df6667462da3f903b6df0171
GotouTamaki <134473633+GotouTamaki@users.noreply.github.com>
2023-06-19 11:26:39 +0900

    UIを修正しました。

--- Assets/Scripts/BGMManager.cs
+++ Assets/Scripts/BGMManager.cs
@@ -12,8 +12,8 @@ public class BGMManager : MonoBehaviour
     [SerializeField]
     AudioClip _BGM_Stage;
 
-    private AudioSource source;
-    private string beforeScene = "StageSelectScene";
+    private AudioSource _source;
+    private string _beforeScene = "StageSelectScene";
     static private BGMManager _instance = null;
 
         // Start is called before the first frame update
@@ -27,11 +27,11 @@ public class BGMManager : MonoBehaviour
         }
 
         _instance = this;
-        source = GetComponent<AudioSource>();
+        _source = GetComponent<AudioSource>();
         DontDestroyOnLoad(gameObject);
         //最初のBGM再生
-        source.clip = _BGM_Title;
-        source.Play();
+        _source.clip = _BGM_Title;
+        _source.Play();
         //シーンが切り替わった時に呼ばれるメソッドを登録
         SceneManager.activeSceneChanged += OnActiveSceneChanged;
     }
@@ -42,43 +42,43 @@ public class BGMManager : MonoBehaviour
         //シーンがどう変わったかで判定
 
         //ステージ選択からチュートリアルへ
-        if (beforeScene == "StageSelectScene" && nextScene.name == "Stage_Tutorial")
+        if (_beforeScene == "StageSelectScene" && nextScene.name == "Stage_Tutorial")
         {
-            source.Stop();
-            source.clip = _BGM_Tutorial; //流すクリップを切り替える
-            source.Play();
+            _source.Stop();
+            _source.clip = _BGM_Tutorial; //流すクリップを切り替える
+            _source.Play();
         }
 
         //ステージ選択からステージへ
-        if (beforeScene == "StageSelectScene" && (nextScene.name == "Stage_1" || nextScene.name == "Stage_2"))
+        if (_beforeScene == "StageSelectScene" && (nextScene.name == "Stage_1" || nextScene.name == "Stage_2"))
         {
-            source.Stop();
-            source.clip = _BGM_Stage;    //流すクリップを切り替える
-            source.Play();
+            _source.Stop();
+            _source.clip = _BGM_Stage;    //流すクリップを切り替える
+            _source.Play();
         }
 
         //ステージからタイトル、ステージ選択へ
-        if ((beforeScene == "Stage_Tutorial" || beforeScene == "Stage_1" || beforeScene == "Stage_2") && (nextScene.name == "TitleScene" || nextScene.name == "StageSelectScene"))
+        if ((_beforeScene == "Stage_Tutorial" || _beforeScene == "Stage_1" || _beforeScene == "Stage_2") && (nextScene.name == "TitleScene" || nextScene.name == "StageSelectScene"))
         {
-            source.Stop();
-            source.clip = _BGM_Title;    //流すクリップを切り替える
-            source.Play();
+            _source.Stop();
+            _source.clip = _BGM_Title;    //流すクリップを切り替える
+            _source.Play();
         }
 
         //ステージからクリア画面へ
-        if ((beforeScene == "Stage_Tutorial" || beforeScene == "Stage_1" || beforeScene == "Stage_2") && nextScene.name == "ClearScene")
+        if ((_beforeScene == "Stage_Tutorial" || _beforeScene == "Stage_1" || _beforeScene == "Stage_2") && nextScene.name == "ClearScene")
         {
-            source.Stop();
+            _source.Stop();
         }
 
         //クリア画面からタイトル、ステージ選択へ
-        if (beforeScene == "ClearScene" && (nextScene.name == "TitleScene" || nextScene.name == "StageSelectScene"))
+        if (_beforeScene == "ClearScene" && (nextScene.name == "TitleScene" || nextScene.name == "StageSelectScene"))
         {
-            source.clip = _BGM_Title; //流すクリップを切り替える
-            source.Play();
+            _source.clip = _BGM_Title; //流すクリップを切り替える
+            _source.Play();
         }
 
         //遷移後のシーン名を「１つ前のシーン名」として保持
-        beforeScene = nextScene.name;
+        _beforeScene = nextScene.name;
     }
 }
